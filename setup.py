c08ab640baf09a6161c07bb4407047302044d643
Yun Xu <yun.xu@airbnb.com>
2019-07-27 00:20:12 -0700

    added entry_points

--- setup.py
+++ setup.py
@@ -17,25 +17,32 @@ install_requires = [
 
 tests_require = []
 
-setup(name='pytest-sanic',
-      version='0.1.0',
-      description='dev tools for Sanic',
-      long_description=open(README_PATH).read(),
-      author='Yun Xu',
-      author_email='yunxu1992@gmail.com',
-      url='https://github.com/yunstanford/sanic-devtools/',
-      packages=find_packages(),
-      install_requires=install_requires,
-      classifiers=[
-          'Development Status :: 5 - Production/Stable',
-          'Operating System :: MacOS',
-          'Operating System :: POSIX :: Linux',
-          'Topic :: System :: Software Distribution',
-          'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.5',
-          'Programming Language :: Python :: 3.6',
-          'Programming Language :: Python :: 3.7',
-      ],
+setup(
+    name='pytest-sanic',
+    version='0.1.0',
+    description='dev tools for Sanic',
+    long_description=open(README_PATH).read(),
+    author='Yun Xu',
+    author_email='yunxu1992@gmail.com',
+    url='https://github.com/yunstanford/sanic-devtools/',
+    packages=find_packages(),
+    install_requires=install_requires,
+    entry_points={
+        "console_scripts": [
+            "sdev=sanic_devtools.cli:cli",
+            "sanic-devtools=sanic_devtools.cli:cli",
+        ]
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX :: Linux',
+        'Topic :: System :: Software Distribution',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
 )
